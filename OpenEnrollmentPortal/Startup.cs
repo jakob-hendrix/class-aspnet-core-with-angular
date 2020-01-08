46e63565b2d4e4487f3a8fb907669849d6f4099b
p00293412 <jakob.hendrix@la.gov>
2020-01-08 16:32:35 -0600

    Finished basic setup. Added file server capabilities

--- OpenEnrollmentPortal/Startup.cs
+++ OpenEnrollmentPortal/Startup.cs
@@ -1,12 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 
 namespace OpenEnrollmentPortal
 {
@@ -21,20 +15,9 @@ namespace OpenEnrollmentPortal
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
-            });
+            // ORDER MATTERS!
+            app.UseDefaultFiles();
+            app.UseStaticFiles();  // for the wwwroot folder
         }
     }
 }

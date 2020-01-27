497d3ac95621f522f73ffe7a598e4696a97de894
jakob-hendrix <jhendrix001@gmail.com>
2020-01-27 07:07:26 -0600

    js events lesson

--- OpenEnrollmentPortal/wwwroot/js/index.js
+++ OpenEnrollmentPortal/wwwroot/js/index.js
@@ -3,3 +3,12 @@ console.log(s);
 
 var theForm = document.getElementById("theForm");
 theForm.hidden = true;
+
+var button = document.getElementById("buyButton");
+button.addEventListener("click", function () {
+  console.log("Buying Item");
+});
+
+var productInfo = document.getElementsByClassName("product-props");
+var listItems = productInfo.item[0].children;
+
